0f451b594d093961edd9857c6b279c320ccd0e18
omkarn@krishworks.com <omkar@krishworks.com>
2025-09-30 19:22:47 +0530

    div in component

--- src/Components/Component.jsx
+++ src/Components/Component.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-
 export const HelloComponent = ({ name }) => {
-  return <h1>HelloComponent, {name}!</h1>;
+  return (
+    <div>
+      <h1>HelloComponent, {name}!</h1>
+    </div>
+  );
 };
